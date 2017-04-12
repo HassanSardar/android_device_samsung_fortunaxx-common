ace7e84978b176bb1422d629f758f8de6f01b3c1
shrukul <shrukul99@gmail.com>
2017-04-12 20:15:37 +0200

    fortunaxx: Fix wireless display
    Change-Id: I0661da3b3e141278ab4dadd5a6d81f89f7b6caf7
    Signed-off-by: Hassan Sardar <hassansardar08@live.com>

---
+++ product/media.mk
@@ -0,0 +1,8 @@
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.aac_51_output_enabled=true
+
+# WiFi Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.debug.wfd.enable=1
+    persist.sys.wfd.virtual=0
