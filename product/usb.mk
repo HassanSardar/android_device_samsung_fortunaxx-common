92043501d6d832af71993f3cf35f5bdc4e8c1d85
Hassan Sardar <hassansardar08@live.com>
2017-11-02 22:19:49 +0000

    fortunaxx: Enable USB Debugging
    Signed-off-by: Hassan Sardar <hassansardar08@live.com>

--- product/usb.mk
+++ product/usb.mk
@@ -2,3 +2,10 @@
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
 	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
+
+# For userdebug builds
+ADDITIONAL_DEFAULT_PROPERTIES += \
+        ro.secure=0 \
+        ro.adb.secure=0 \
+        ro.debuggable=1 \
+        persist.service.adb.enable=1
