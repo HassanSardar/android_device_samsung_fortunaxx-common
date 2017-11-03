4c829af23d8bd860cbf535b741c52f5f13d642e8
Hassan Sardar <hassansardar08@live.com>
2017-11-03 21:12:57 +0000

    fortunaxx: perf: Set I/O scheduler to bfq
    Signed-off-by: Hassan Sardar <hassansardar08@live.com>

--- product/perf.mk
+++ product/perf.mk
@@ -1,3 +1,7 @@
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.vendor.extension_library=/system/vendor/lib/libqti-perfd-client.so
+
+# I/O scheduler
+PRODUCT_PROPERTY_OVERRIDES += \
+	sys.io.scheduler=bfq
